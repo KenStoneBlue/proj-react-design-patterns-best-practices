95bba61cbb2905cbd568b0d6e3cfd85b04d13c34
Ken Stone <ken@stoneblue.com>
2020-04-27 15:04:04 -0500

    new create react app project and layout

---
+++ layouts/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Header from './components/layout/Header';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render() {
+    return (
+      <div className="App">
+        <Header title="Stone Blue" />
+
+      </div>
+    );
+  }
+}
+
+export default App;
